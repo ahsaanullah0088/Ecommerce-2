5a5da8ae1a59bf92e169ff92435cb3f018585d65
Ahsaan Ullah <ahsaanu70@gmail.com>
2025-06-30 16:08:58 +0500

    Implementing auth logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,27 +14,41 @@ import ShopingHome from './pages/shoping-view/home'
 import ShopingListing from './pages/shoping-view/listing'
 import ShopingCheckout from './pages/shoping-view/checkout'
 import ShopingAccount from './pages/shoping-view/account'
-
-
-
+import CheckAuth from './components/common/check-auth'
+import UnauthPage from './pages/unauth-page'
 
 
 const App = () => {
+  const isAuthenticated = true; //
+  // u Replace with actual authentication logic
+  // const user = null;
+  const user = {
+    name : "John Doe",
+    role : "user",
+
+
+  } // Replace with actual user data
   return (
     <div className='flex flex-col overflow-hidden bg-white'>
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route path="/auth" element={<CheckAuth isAuthenticated={isAuthenticated} user={user}>
+          <AuthLayout />
+        </CheckAuth>}>
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
-        <Route path = "/admin" element = {<AdminLayout/>}>
+        <Route path = "/admin" element = {<CheckAuth isAuthenticated={isAuthenticated} user={user}>
+          <AdminLayout/>
+        </CheckAuth>}>
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="product" element={<AdminProducts />} />
           <Route path="sidebar" element={<AdminSidebar />} />
           <Route path="orders" element={<AdminOrders />} />
         
         </Route>
-        <Route path = "/shop" element = {<ShopingLayout/>}>
+        <Route path = "/shop" element = {<CheckAuth isAuthenticated={isAuthenticated} user={user}>
+          <ShopingLayout />
+        </CheckAuth>}>
           <Route path="home" element={<ShopingHome />} />
           <Route path="listing" element={<ShopingListing />} />
           <Route path="checkout" element={<ShopingCheckout />} />
@@ -43,6 +57,7 @@ const App = () => {
 
         </Route>
       <Route path = "*" element = {<NotFound/>} />
+      <Route path = "/unauth" element = {<UnauthPage/>}/>
 
       </Routes>
     </div>
